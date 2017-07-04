44f0ffb81939bd521960a6d44a062b9cba20f2ff
James Kiesel <james.kiesel@gmail.com>
2017-07-04 16:03:37 +1200

    Add referrals to dev route

--- app/controllers/dev_controller.rb
+++ app/controllers/dev_controller.rb
@@ -19,14 +19,22 @@ class DevController < ActionController::Base
 
   def setup_node_tree
     Node.create(title: Node::INITIAL_NODE_TITLE).tap do |root|
-      Node.create(title: "Question A", parent_node_id: root.id).tap do |a|
-        Node.create(title: "Question A -> C", parent_node_id: a.id,  terminal: true)
-        Node.create(title: "Question A -> D", parent_node_id: a.id,  terminal: true)
+      Node.create(title: "Node A", parent_node_id: root.id).tap do |a|
+        Node.create(title: "Node A1", parent_node_id: a.id,  terminal: true).tap do |a1|
+          a1.referrals.push(Referral.create(type: "PrimaryReferral", title: "Referral A1"))
+        end
+        Node.create(title: "Node A2", parent_node_id: a.id,  terminal: true).tap do |a2|
+          a2.referrals.push(Referral.create(type: "PrimaryReferral", title: "Referral A2"))
+        end
       end
 
-      Node.create(title: "Question B", parent_node_id: root.id).tap do |b|
-        Node.create(title: "Question B -> C", parent_node_id: b.id,  terminal: true)
-        Node.create(title: "Question B -> D", parent_node_id: b.id,  terminal: true)
+      Node.create(title: "Node B", parent_node_id: root.id).tap do |b|
+        Node.create(title: "Node B1", parent_node_id: b.id,  terminal: true).tap do |b1|
+          b1.referrals.push(Referral.create(type: "PrimaryReferral", title: "Referral B1"))
+        end
+        Node.create(title: "Node B2", parent_node_id: b.id,  terminal: true).tap do |b2|
+          b2.referrals.push(Referral.create(type: "PrimaryReferral", title: "Referral B2"))
+        end
       end
     end
   end
--- app/models/node.rb
+++ app/models/node.rb
@@ -3,7 +3,7 @@ class Node < ApplicationRecord
   has_many :children, class_name: "Node", foreign_key: :parent_node_id
   has_many :referrals, foreign_key: :terminal_node_id
 
-  INITIAL_NODE_TITLE = "Select a county".freeze
+  INITIAL_NODE_TITLE = "Initial Node".freeze
 
   def self.initial
     Node.find_by(title: INITIAL_NODE_TITLE) # TODO: improve
