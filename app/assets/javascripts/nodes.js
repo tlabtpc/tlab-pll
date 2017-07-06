e20b3cb6d6728bb6e5fabac69cb3ff7b3320c0e0
James Kiesel <james.kiesel@gmail.com>
2017-07-07 10:55:24 +1200

    Use var instead of let

--- app/assets/javascripts/nodes.js
+++ app/assets/javascripts/nodes.js
@@ -1,6 +1,6 @@
 $(function() {
   $('.nodes-show').on('click', '.nodes__child-list-item', function(e) {
-    let target = $(e.target)
+    var target = $(e.target)
     $('.nodes__child-list-item').removeClass('nodes__child-list-item--selected')
     target.addClass('nodes__child-list-item--selected')
 
