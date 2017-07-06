e4245bbdc7d3ec1b98a73c4d595e7d6bf029bed5
James Kiesel <james.kiesel@gmail.com>
2017-07-07 10:08:44 +1200

    Use label as form submit (!!)

--- app/assets/javascripts/assessments.js
+++ app/assets/javascripts/assessments.js
@@ -1,6 +1,7 @@
 $(function() {
   $('.assessments-new').on('change', '.assessments__checkbox.confirm', function() {
     allChecked = $('.assessments__checkbox.confirm').toArray().every(function(c) { return c.checked })
-    $('.assessments__next-button').prop('disabled', !allChecked)
+    $('.assessments__submit').prop('disabled', !allChecked)
+    $('.assessments__next-button').toggleClass('disabled', !allChecked)
   })
 })
--- app/views/assessments/new.html.rb
+++ app/views/assessments/new.html.rb
@@ -1,24 +1,39 @@
 class Views::Assessments::New < Views::Base
   def content
-    columns("small-centered", medium: 8, large: 7) do
-      h4 'Before we start, please confirm the following:'
+    form_for :assessment, url: assessments_path do |f|
+      columns("small-centered", medium: 8, large: 7) do
+        h4 'Before we start, please confirm the following:'
 
-      assessment_check 'My client and I have enough time to work on this now.',
-        'If not today, try to schedule a date and time to follow-up with your client.',
-        'schedule'
+        assessment_check 'My client and I have enough time to work on this now.',
+          'If not today, try to schedule a date and time to follow-up with your client.',
+          'schedule'
 
-      assessment_check 'I have asked my client if he/she has any paperwork related to this issue.',
-        'Documents from a court, government agency, or attorney often include response times, appeal deadlines, and other important information.',
-        'paperwork'
+        assessment_check 'I have asked my client if he/she has any paperwork related to this issue.',
+          'Documents from a court, government agency, or attorney often include response times, appeal deadlines, and other important information.',
+          'paperwork'
 
-      assessment_check 'I understand that Project Legal Link does not provide legal representation for my client.',
-        'This tool does not collect client-identifying information and does not create any attorney-client relationships.',
-        'legal'
+        assessment_check 'I understand that Project Legal Link does not provide legal representation for my client.',
+          'This tool does not collect client-identifying information and does not create any attorney-client relationships.',
+          'legal'
+      end
+
+      f.submit(class: "assessments__submit", id: :assessment_submit, disabled: true)
+    end
+
+    content_for :footer do
+      div(class: "assessments__footer") do
+        div
+        label(class: "button button--green assessments__next-button disabled", for: :assessment_submit) do
+          span "Next"
+          i class: "fa fa-arrow-right"
+        end
+        div
+      end
     end
   end
 
   def assessment_check(title, subtitle, id)
-    input(class: "checkbox assessments__checkbox", id: "intro[#{id}]", type: "checkbox", name: "intro[#{title}]")
+    input(class: "checkbox assessments__checkbox confirm", id: "intro[#{id}]", type: "checkbox", name: "intro[#{title}]")
     label(for: "intro[#{id}]") do
       div(class: "label--check") { text "✓" }
       div(class: "label--text") do
@@ -26,5 +41,8 @@ class Views::Assessments::New < Views::Base
         p subtitle
       end
     end
+  end
+
+  def special_referral_check(referral)
   end
 end
