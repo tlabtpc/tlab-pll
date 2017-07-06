23cd1f48c8d36532ad9e9380961ee6dfc80915af
Heather Moore and James Kiesel <heather+james@citizencode.io>
2017-07-06 15:43:19 -0700

    heroku not into es6

--- app/assets/javascripts/nodes.js
+++ app/assets/javascripts/nodes.js
@@ -1,11 +1,11 @@
 $(function() {
   $('.nodes-show').on('click', '.nodes__child-list-item', function(e) {
-    let target = $(e.target)
-    $('.nodes__child-list-item').removeClass('nodes__child-list-item--selected')
-    target.addClass('nodes__child-list-item--selected')
+    var target = $(e.target);
+    $('.nodes__child-list-item').removeClass('nodes__child-list-item--selected');
+    target.addClass('nodes__child-list-item--selected');
 
-    $('.nodes__submit-button').prop('disabled', false)
-    $('#node_id').val(target.data('id'))
+    $('.nodes__submit-button').prop('disabled', false);
+    $('#node_id').val(target.data('id'));
     $('#node_description').val(target.data('description'))
   })
-})
+});
