12b6e46c63b8e9be9f9bd0e890c30a36a32de6a8
James Kiesel <james.kiesel@gmail.com>
2017-07-07 12:17:28 +1200

    Only show special referrals panel once confirmations have been checked

--- app/assets/javascripts/assessments.js
+++ app/assets/javascripts/assessments.js
@@ -1,7 +1,14 @@
 $(function() {
   $('.assessments-new').on('change', '.assessments__checkbox.confirm', function() {
-    allChecked = $('.assessments__checkbox.confirm').toArray().every(function(c) { return c.checked })
+    var allChecked = $('.assessments__checkbox.confirm').toArray().every(function(c) { return c.checked })
+
+    // enable the (hidden) submit button
     $('.assessments__submit').prop('disabled', !allChecked)
+
+    // enable the next label
     $('.assessments__next-button').toggleClass('disabled', !allChecked)
+
+    // show the special referrals panel
+    $('.assessments__special-referrals').toggle(allChecked)
   })
 })
