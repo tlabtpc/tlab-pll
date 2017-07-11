6a19eacb122d9304a5d6cdb08529087d56a651da
Heather Moore and James Kiese <heather+james@citizencode.io>
2017-07-11 12:00:21 +1200

    Initial passing integration spec

--- spec/features/assessment_spec.rb
+++ spec/features/assessment_spec.rb
@@ -1,24 +1,33 @@
 require "rails_helper"
 
 describe "assessment", js: true do
-  let(:special_referral) { create(:special_referral) }
+  let!(:special_referral) { create(:special_referral) }
 
   specify do
     visit root_path
-    puts page.html
 
     step "agree to initial requirements" do
-      find(:xpath, '//label[for=confirm_schedule]').click
-      select "#confirm_paperwork"
+      click_for "confirm_schedule"
+      click_for "confirm_paperwork"
 
       expect(page).to_not have_css ".assessments__special-referrals"
-      select "#confirm_legal"
+      click_for "confirm_legal"
 
       expect(page).to have_css ".assessments__special-referrals"
 
-      select "#referral_id_#{special_referral.id}"
+      click_for "referral_id_#{special_referral.id}"
 
-      click_button "Next"
+      click_for "assessment_submit"
     end
+
+    step "select county node" do
+    end
+
+    step "select category node" do
+    end
+  end
+
+  def click_for(for_value)
+    find("label[for=#{for_value}]").click
   end
 end
