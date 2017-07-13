163dfc8504881886028c3f190934097efde5aa7c
Bion Johnson <bion@citizencode.io>
2017-07-13 14:13:41 -0700

    Fix next button disabled behavior for cross check details

--- app/assets/javascripts/cross-checks.js
+++ app/assets/javascripts/cross-checks.js
@@ -1,16 +1,26 @@
 $(function() {
-  if ($('body').find('.cross-checks__textarea--required').length) {
-    $('.button--submit').addClass('button--disabled')
-  }
+  var $cross_check_submit = $('#cross_check_submit');
 
   $('body').on('keyup', '.cross-checks__textarea--required', function(e) {
-    var target = $(e.target)
-    var val = target.val()
+    var target = $(e.target);
+    var val = target.val();
+
     if (val.length) {
-      var description = target.data('description')
+      var description = target.data('description');
     }
 
-    $('.button--submit').toggleClass('button--disabled', !!!val.length)
-    $('.footer-flash').html(description || "").toggleClass('footer-flash--visible', !!description)
+    var disabled = !val.length;
+
+    $('.button--submit')
+      .toggleClass('button--disabled', disabled)
+      .prop('disabled', disabled);
+
+    $cross_check_submit.prop('disabled', disabled);
+
+    $('.footer-flash')
+      .html(description || "")
+      .toggleClass('footer-flash--visible', Boolean(description));
   })
+
+  $('.cross-checks__textarea--required').trigger('keyup');
 })
--- app/views/cross_checks/helper.rb
+++ app/views/cross_checks/helper.rb
@@ -2,7 +2,9 @@ module Views::CrossChecks::Helper
   def cross_check_form
     form_for :cross_check, url: next_step_cross_checks_path do |f|
       hidden_field_tag :current_step, action_name
+
       yield(f) if block_given?
+
       f.submit(class: :hide, id: :cross_check_submit)
     end
   end
