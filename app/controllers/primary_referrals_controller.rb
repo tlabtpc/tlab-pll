2614b1ab581b2ada4d922acf7f34f577f8aa3eaa
James Kiesel <james@citizencode.io>
2017-07-14 15:00:49 +1200

    Remove extra layout classes, add tip to assessreferrals

--- app/controllers/primary_referrals_controller.rb
+++ app/controllers/primary_referrals_controller.rb
@@ -2,6 +2,6 @@ class PrimaryReferralsController < ApplicationController
   skip_before_action :basic_auth, :verify_allowed_user
 
   def show
-    @primary_referral = PrimaryReferral.find(params[:id])
+    @primary_referral = Referral.non_secondary.find(params[:id])
   end
 end
--- app/models/referral.rb
+++ app/models/referral.rb
@@ -1,5 +1,6 @@
 class Referral < ApplicationRecord
   belongs_to :terminal_node, required: false, class_name: "Node"
+  scope :non_secondary, -> { where.not(type: "SecondaryReferral") }
 
   scope :primary,   -> { where(type: "PrimaryReferral") }
   scope :secondary, -> { where(type: "SecondaryReferral") }
