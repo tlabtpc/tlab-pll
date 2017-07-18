25a7b8d5337b03059eae7f420550c6e582a7c064
Heather Moore and James Kiesel <heather+james@citizencode.io>
2017-07-18 08:58:33 -0400

    update caseworker form labels

--- app/views/cross_checks/info.html.rb
+++ app/views/cross_checks/info.html.rb
@@ -6,10 +6,10 @@ class Views::CrossChecks::Info < Views::Base
     content_for :card do
       h4 "Please provide the following:"
       cross_check_form do |f|
-        f.label :name, for: :cross_check_first_name
+        f.label 'Your first name', for: :cross_check_first_name
         f.text_field :first_name
 
-        f.label :name, for: :cross_check_last_name
+        f.label 'Your last name', for: :cross_check_last_name
         f.text_field :last_name
 
         f.label :phone, for: :cross_check_caseworker_phone
