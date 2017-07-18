dabd8a4b45e0c6ae161c993901b8e0671b3c8455
David Balatero and Heather Moore <david+heather@citizencode.io>
2017-07-18 14:45:48 -0400

    fix duped copyright

--- app/views/application/_copyright.html.rb
+++ app/views/application/_copyright.html.rb
@@ -2,7 +2,7 @@ class Views::Application::Copyright < Views::Base
   def content
     div class: :copyright do
       mail_to "legallink@baylegal.org", "legallink@baylegal.org"
-      text " • 415.851.1PLL (415-851-1755)"
+      text " • 415.851.1PLL"
       text " (415.851.1755) • © #{Date.today.year} Project Legal Link. All Rights Reserved."
     end
   end
