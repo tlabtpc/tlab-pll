fb23061e8d1b2c14660bf0cbae0749d7a9824aaa
James Kiesel <james@citizencode.io>
2017-07-20 15:52:07 +1200

    Align top bar

--- app/views/widgets/top_bar.rb
+++ app/views/widgets/top_bar.rb
@@ -1,13 +1,14 @@
 class Views::Widgets::TopBar < Views::Base
-  # http://foundation.zurb.com/sites/docs/top-bar.html
-
   needs :title
 
   def content
     div class: "top-bar sticky" do
-      div class: "top-bar-left" do
-        image_tag "logo.png", alt: title
-        p "Help your client take action with support from Project Legal Link"
+      div class: "top-bar__content" do
+        div do
+          image_tag "logo.png", alt: title
+          p "Help your client take action with support from Project Legal Link"
+        end
+        div
       end
     end
   end
