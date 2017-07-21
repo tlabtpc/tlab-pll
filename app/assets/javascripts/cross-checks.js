35c0dd51632ca864a4de84a6c4e89e68b17e66cb
James Kiesel <james@citizencode.io>
2017-07-21 15:33:07 +1200

    Fix failing spec

--- app/assets/javascripts/cross-checks.js
+++ app/assets/javascripts/cross-checks.js
@@ -7,15 +7,17 @@ $(function() {
       return $(this).val().length <= 0
     }).length)
 
-    if (!elementsNeedInput) {
-      description = $(e.target).data('description')
-    }
-
     $('.button--submit')
       .toggleClass('button--disabled', elementsNeedInput)
       .prop('disabled', elementsNeedInput)
 
     $crossCheckSubmit.prop('disabled', elementsNeedInput)
+  }
+
+  displayDescription = function(e) {
+    if ($(this).val().length) {
+      var description = $(this).data('description')
+    }
 
     $('.footer-flash')
       .html(description || "")
@@ -23,6 +25,8 @@ $(function() {
   }
 
   $('body').on('keyup', '.cross-checks__input--required', verifyRequiredFields)
+  $('body').on('keyup', '.cross-checks__textarea[data-description]', displayDescription)
   $('body').on('change', 'select.cross-checks__input--required', verifyRequiredFields)
   $('.cross-checks__input--required').trigger('keyup');
+  $('.cross-checks__textarea').trigger('keyup');
 })
