1fc26611844d2c774d02a8de8f25837dbbda3522
Heather Moore <heather@citizencode.io>
2017-07-24 14:12:28 -0400

    update assessment tip

--- app/views/tips/_assessment.html.rb
+++ app/views/tips/_assessment.html.rb
@@ -2,15 +2,14 @@ class Views::Tips::Assessment < Views::Base
   def content
     h4 class: 'tips__header' do
       i class: "fa fa-lightbulb-o"
-      span "If your client is self-represented..."
+      span "If your client is self-represented:"
     end
-    # TODO Look at content doc for this
-    p "then coach your client to be on time + meet deadlines, get help with paperwork at self-help center. Respectfully self-advocate"
+    p "Coach your client to be on time + meet deadlines, get help with paperwork at self-help center. Respectfully self-advocate"
 
     h4 class: 'tips__header' do
       i class: "fa fa-lightbulb-o"
-      span "If your client has an attorney for any case..."
+      span "If your client already has an attorney:"
     end
-    p "Then that attorney is the best resource for information on that case (including a public defender for a criminal case)."
+    p "That attorney is the best resource for information on that case (including a public defender for a criminal case)."
   end
 end
