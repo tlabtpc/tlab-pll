25592d73d6d2890d626bafe2a7a80dead3f35d0e
Heather Moore <heather@citizencode.io>
2017-07-25 18:48:07 -0400

    make www.immi.org a link in the cw tip

--- app/views/tips/_immigration_sf_one.html.rb
+++ app/views/tips/_immigration_sf_one.html.rb
@@ -1,7 +1,11 @@
 class Views::Tips::ImmigrationSfOne < Views::Base
   def content
     render "tips/caseworker_header"
-    p "If the client is not sure what type of relief she might be eligible for, she can use the screening tool at www.immi.org to find out."
+    p do
+      text "If the client is not sure what type of relief she might be eligible for, she can use the screening tool at "
+      link_to "www.immi.org", "www.immi.org"
+      text " to find out."
+    end
     render "tips/caseworker_header"
     p "If your client has an immigration court date and does not have an attorney, she should try to find counsel immediately (especially if she is in removal proceedings)."
   end
