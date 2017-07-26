7c70dcaff1a9a3c518ea9621f0c7955f42ba2c65
Heather Moore <heather@citizencode.io>
2017-07-26 14:02:43 -0400

    allow admins to change title of secondary referrals

--- app/dashboards/secondary_referral_dashboard.rb
+++ app/dashboards/secondary_referral_dashboard.rb
@@ -41,6 +41,7 @@ class SecondaryReferralDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
+    :title,
     :link,
   ].freeze
 
