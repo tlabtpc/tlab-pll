75d6488c3365e86c5559b9070ef6d233ede08fde
Heather Moore <heather@citizencode.io>
2017-08-03 08:02:37 -0400

    add bcc to mail interceptor

--- app/lib/pre_production_mail_interceptor.rb
+++ app/lib/pre_production_mail_interceptor.rb
@@ -1,6 +1,6 @@
 class PreProductionMailInterceptor
   def self.delivering_email(message)
-    message.subject = "[#{message.to}] #{message.subject}"
+    message.subject = "to:[#{message.to}] bcc:[#{message.bcc}] #{message.subject}"
     message.to = ENV['MAIL_INTERCEPTOR_DESTINATION'] ||
       "projectlegallink@mailinator.com"
   end
