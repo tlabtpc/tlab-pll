852f5008d6477e67c3991adc09dde77549f93a3c
Heather Moore <heather@citizencode.io>
2017-08-03 09:17:33 -0400

    remove cc and bcc on intercepted emails

--- app/lib/pre_production_mail_interceptor.rb
+++ app/lib/pre_production_mail_interceptor.rb
@@ -1,7 +1,9 @@
 class PreProductionMailInterceptor
   def self.delivering_email(message)
-    message.subject = "to:[#{message.to}] bcc:[#{message.bcc}] #{message.subject}"
+    message.subject = "to:[#{message.to}] cc:[#{message.cc}] bcc:[#{message.bcc}] #{message.subject}"
     message.to = ENV['MAIL_INTERCEPTOR_DESTINATION'] ||
       "projectlegallink@mailinator.com"
+    message.bcc = nil
+    message.cc = nil
   end
 end
