954da71a5e0665ff182bc74eed1c98b647bffa60
Heather Moore <heather@citizencode.io>
2017-08-03 07:50:35 -0400

    add bcc to assessments mailer

--- app/mailers/assessments_mailer.rb
+++ app/mailers/assessments_mailer.rb
@@ -4,6 +4,6 @@ class AssessmentsMailer < ApplicationMailer
   def show(assessment, to:, cross_check: false)
     @assessment = assessment.decorate
     @cross_check = cross_check
-    mail(to: to, subject: "Summary assessment ##{@assessment.to_param}")
+    mail(to: to, bcc: ENV['ASSESSMENT_MAIL_BCC'], subject: "Summary assessment ##{@assessment.to_param}")
   end
 end
---
+++ spec/mailers/assessments_mailer_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe AssessmentsMailer do
+  let(:assessment) { create(:assessment) }
+
+  describe "#show" do
+    it "sends an email the case worker and bccs the assessment reviewer from PLL" do
+      allow(ENV).to receive(:[]).with("ASSESSMENT_MAIL_BCC").and_return("sacha@example.com")
+
+      email = AssessmentsMailer.show(
+        assessment,
+        to: "caseworker@example.com",
+        cross_check: true
+      )
+
+      expect(email.from).to eq ['no-reply@projectlegallink.org']
+      expect(email.to).to eq ['caseworker@example.com']
+      expect(email.bcc).to eq ['sacha@example.com']
+      expect(email.subject).to eq "Summary assessment ##{assessment.to_param}"
+    end
+  end
+end
